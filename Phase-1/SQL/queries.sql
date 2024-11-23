35d1257d3b39920462b81570e336bb9f4ac3db10
TheMasterProgrammer <ryanrebbas2001@gmail.com>
2024-11-22 22:18:33 -0500

    Update queries.sql

--- Phase-1/SQL/queries.sql
+++ Phase-1/SQL/queries.sql
@@ -1,8 +1,10 @@
+-- Basic select with simple where clause
 -- Select all articles from article_worldNewsAPI with a sentiment score of 0.8 or higher
 SELECT *
 FROM article_worldNewsAPI
 WHERE sentiment >= 0.8;
 
+-- Basic select with simple group by clause (with and without having clause)
 -- Group articles by category and count the number of articles
 SELECT category, COUNT(*) AS article_count
 FROM article_worldNewsAPI
@@ -14,7 +16,7 @@ FROM article_worldNewsAPI
 GROUP BY category
 HAVING COUNT(*) > 5;
 
-
+-- A simple join query as well as its equivalent implementation using cartesian product and where clause.
 -- Match and retrieve articles published on the same day from two different sources using join.
 SELECT 
     n.id AS newsapi_article_id,                
@@ -43,6 +45,48 @@ FROM
     article_worldNewsAPI w
 WHERE 
     DATE(n.publish_date) = DATE(w.publish_date);
+
+-- A few queries to demonstrate various join types on the same tables: inner vs. outer (left and right) vs. full join. 
+--Inner join
+SELECT
+    w.id AS worldnews_id,
+    w.title AS worldnews_title,
+    n.id AS newsapi_id,
+    n.title AS newsapi_title
+FROM
+    article_worldNewsAPI w
+INNER JOIN
+    article_newsAPI n ON LEFT(w.title, 3) = LEFT(n.title, 3);
+--Left outer join
+SELECT
+    w.id AS worldnews_id,
+    w.title AS worldnews_title,
+    n.id AS newsapi_id,
+    n.title AS newsapi_title
+FROM
+    article_worldNewsAPI w
+LEFT JOIN
+    article_newsAPI n ON LEFT(w.title, 3) = LEFT(n.title, 3);
+--Right outer join
+SELECT
+    w.id AS worldnews_id,
+    w.title AS worldnews_title,
+    n.id AS newsapi_id,
+    n.title AS newsapi_title
+FROM
+    article_worldNewsAPI w
+RIGHT JOIN
+    article_newsAPI n ON LEFT(w.title, 3) = LEFT(n.title, 3);
+--Full outer join
+SELECT
+    w.id AS worldnews_id,
+    w.title AS worldnews_title,
+    n.id AS newsapi_id,
+    n.title AS newsapi_title
+FROM
+    article_worldNewsAPI w
+FULL OUTER JOIN
+    article_newsAPI n ON LEFT(w.title, 3) = LEFT(n.title, 3);
 
 
 
