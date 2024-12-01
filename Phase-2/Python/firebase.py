8d481ec5d9a62ccd0385d551606694351a03100d
TheMasterProgrammer <ryanrebbas2001@gmail.com>
2024-11-30 19:31:51 -0500

    Update firebase.py

--- Phase-2/Python/firebase.py
+++ Phase-2/Python/firebase.py
@@ -15,3 +15,20 @@ def populate_subcollection(subcollection_name, articles):
         db.collection(subcollection_name).add(article)
         print(f"Added article {doc_id} to {subcollection_name}")
 
+def search_by_country(country_code):
+    query = db.collection("articlesWorldnewsAPI").where("source_country", "==", country_code)
+    res = query.stream()
+    data = []
+    for doc in res:
+        data.append(doc.to_dict())
+    print("Articles with source country: {}".format(country_code))
+    print(data)
+
+def count_by_sentiment(threshold):
+    query = db.collection("articlesWorldnewsAPI").where("sentiment", ">=", threshold).count()
+    count_result = query.get()  
+
+    for aggregation_list in count_result:  
+        for aggregation in aggregation_list:  
+            print(f"Number of articles with sentiment >= {threshold}: {aggregation.value}")
+       
