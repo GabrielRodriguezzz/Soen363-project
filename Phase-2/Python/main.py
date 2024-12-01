2539cbf4c564b6df7dcc4028d22e195db7f937c3
TheMasterProgrammer <ryanrebbas2001@gmail.com>
2024-11-30 19:32:11 -0500

    Update main.py

--- Phase-2/Python/main.py
+++ Phase-2/Python/main.py
@@ -1,12 +1,16 @@
 from fetchDB import fetch_all_articles
-
+from firebase import search_by_country
+from firebase import count_by_sentiment
 
 
 
 if __name__ == "__main__":
     #populate firebase db
     #fetch_all_articles()
-    
+    #A basic search query on an attribute value.
+    #search_by_country("ca")
+    #A query that provides some aggregate data (i.e. number of entities satisfying a criteria)
+    #count_by_sentiment(0.6)
     print("")
 
 
